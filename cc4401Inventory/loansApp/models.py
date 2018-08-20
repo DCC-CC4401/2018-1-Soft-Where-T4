63336aab2bdd919f3bb20d798836373b61aa98af
Pedro Belmonte <pedro.belmonte@ing.uchile.cl>
2018-08-20 17:38:47 -0300

    administracion de reservas y prestamos

--- cc4401Inventory/loansApp/models.py
+++ cc4401Inventory/loansApp/models.py
@@ -5,4 +5,5 @@ from django.db import models
 
 class Loan(Action):
     article = models.ForeignKey(Article, on_delete=models.CASCADE)
-    admin = models.ForeignKey(User, related_name="admin_art_prestador", on_delete=models.CASCADE, default=None)
+    admin = models.ForeignKey(User, related_name="admin_art_prestador", on_delete=models.CASCADE, default=None,
+                              null=True)
