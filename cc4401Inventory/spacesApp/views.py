6e5bd12ca4ccb53b7d46dfabd4810e3de86d4d20
Matipedia <maty.llegas@gmail.com>
2018-08-20 00:56:31 -0300

    Agregadas paginas de espacios, con links desde admin

--- cc4401Inventory/spacesApp/views.py
+++ cc4401Inventory/spacesApp/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from spacesApp.models import Space
+from reservationsApp.models import Reservation
 
-# Create your views here.
+from datetime import datetime, timedelta
+
+import random, os
+import pytz
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
+
+@login_required
+def space_data(request, space_id):
+    try:
+        espacio = Space.objects.get(id=space_id)
+
+        last_reservations = Reservation.objects.filter(space=espacio,
+                                                       ending_date_time__lt=datetime.now(tz=pytz.utc)
+                                                       ).order_by('-ending_date_time')[:10]
+
+        reservation_list = list()
+        for reservation in last_reservations:
+
+            starting_day = reservation.starting_date_time.strftime("%d-%m-%Y")
+            ending_day = reservation.ending_date_time.strftime("%d-%m-%Y")
+            starting_hour = reservation.starting_date_time.strftime("%H:%M")
+            ending_hour = reservation.ending_date_time.strftime("%H:%M")
+
+            if starting_day == ending_day:
+                reservation_list.append(starting_day + " " + starting_hour + " a " + ending_hour)
+            else:
+                reservation_list.append(starting_day + ", " + starting_hour + " a " + ending_day + ", " + ending_hour)
+
+        context = {
+            'espacio': espacio,
+            'last_reservations': reservation_list
+        }
+
+        return render(request, 'space_data.html', context)
+    except Exception as e:
+        print(e)
+        return redirect('/')
